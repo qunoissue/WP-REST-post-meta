ebb55a7fb2ad502533891802361e814a535e27bc
Kadzuya OKAMOTO <arow.okamoto@gmail.com>
2016-06-20 15:46:24 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# WP REST API extra routes for post meta
+
+Extra routes for WP REST API2 to manage post meta data.
+Now, only one route to get pair list of specific meta data and post id.
+
+Requires WP API Version 2.0 Beta 13.1 or higher.
+
+## Usage
+
+```
+$ curl --user "$username:$password" -X GET http://example.com/wp-json/meta_val/v1/posts/some_param/-1 | jq
+[
+  {
+    "some_param": "34",
+    "post_id": 24689
+  },
+  {
+    "some_param": "33",
+    "post_id": 26826
+  },
+  ...
+]
+```
---
+++ wp-api-meta-data.php
@@ -0,0 +1,55 @@
+<?php
+/*
+Plugin Name:  WP REST API extra routes for post meta
+Plugin URI:   https://github.com/arowM/WP-REST-post-meta
+Description:  Extra routes for WP REST API2 to manage post meta data
+Version:      0.0.1
+Author:       Kadzuya Okamoto
+Author URI:   http://www.arow.info
+License:      MIT License
+*/
+
+namespace WPAPIPostMeta;
+
+function get_meta_val_list_init() {
+  /**
+   * Grab meta_val and post_id list.
+   *
+   * @param array $data Options for the function.
+   * @return array of meta_val and post_id pair.
+   */
+  function get_meta_val_list($data) {
+    $meta_val = 'job_id';
+    $meta_val = $data['meta_val'];
+    $args = array(
+      'numberports' => $numports,
+    );
+    return array_filter(
+      array_map(function($a) use ($meta_val) {
+        $pid = $a->ID;
+        $jids = \get_post_meta($pid, $meta_val, false);
+        return array_reduce($jids, function($ls, $jid) use ($pid, $meta_val) {
+          return array_merge(array(
+            $meta_val => $jid,
+            'post_id' => $pid,
+          ), $ls);
+        }, array());
+      }, \get_posts($args)),
+      function($a) {
+        return $a !== array();
+      }
+    );
+  }
+
+  \add_action('rest_api_init', function () {
+    \register_rest_route('meta_val/v1', '/posts/(?P<meta_val>.+)/(?P<numports>[0-9\-]+)',
+    // \register_rest_route('meta_val/v1', '/meta_vals',
+      array(
+  		  'methods' => 'GET',
+        'callback' => __NAMESPACE__ . '\\get_meta_val_list',
+      )
+    );
+  });
+}
+
+add_action( 'init', __NAMESPACE__ . '\\get_meta_val_list_init' );
